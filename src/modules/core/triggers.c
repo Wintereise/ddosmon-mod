Wintereise/ddosmon-mod

commit e075e3ccec7387f801333f1c627e9f5f72987267

modules/core/triggers.c: add per-block expiry.

src/modules/core/triggers.c
@@ -28,6 +28,7 @@ typedef struct _trigger {
 	unsigned int target_mbps;
 	unsigned int target_flowcount;
 	unsigned int below_mbps;
+	unsigned int expiry;
 	unsigned char protocol;
 	unsigned char tcp_synonly;
 
@@ -123,6 +124,9 @@ expire_triggers(void)
 		patricia_node_t *node;
 
 		if (get_time() < (rec->added + expiry))
+			continue;
+
+		if (!rec->t->expiry || get_time() < (rec->added + rec->t->expiry))
 			continue;
 
 		run_triggers(ACTION_UNBAN, rec->t, &rec->pkt, &rec->irec);
@@ -248,6 +252,8 @@ parse_trigger(config_entry_t *entry)
 			t->below_mbps = atoi(ce->ce_vardata);
 		else if (!strcasecmp(ce->ce_varname, "target_pps"))
 			t->target_pps = atoi(ce->ce_vardata);
+		else if (!strcasecmp(ce->ce_varname, "expiry"))
+			t->expiry = atoi(ce->ce_vardata);
 		else if (!strcasecmp(ce->ce_varname, "actions"))
 			parse_actions(t, ce->ce_entries);
 	}

dist/ddosmon.conf
@@ -96,12 +96,19 @@ module "source/pcap" {
 module "core/triggers" {
 	// the number of seconds that between triggering an action and triggering
 	// the reversion of said action.  in other words, the expiry for nullroutes.
+	// This overrides per-trigger expiries (that is, if it is sooner than the
+	// per-trigger expiries, it will expire them first)
 	expiry = 300;
 
 	/* trigger a nullroute if a UDP flood larger than 120mbps occurs. */
 	trigger {
 		protocol = udp;
 		target_mbps = 120;
+
+                // Expire this trigger - only has effect if sooner than the "global"
+                // expiry. Left undefined, it will have no effect.
+		expiry = 150;
+
 		actions {
 			cisco-nullroute;
 			email;
@@ -112,6 +119,7 @@ module "core/triggers" {
 	trigger {
 		protocol = icmp;
 		target_mbps = 120;
+		expiry = 150;
 		actions {
 			cisco-nullroute;
 			email;
@@ -124,6 +132,7 @@ module "core/triggers" {
 	trigger {
 		protocol = tcp;
 		target_pps = 350000;
+		expiry = 150;
 		actions {
 			cisco-nullroute;
 			email;

commit 11144fce998cbe39bd5a57739c5a92ce7b4c49d0

core/triggers: fix expiry of triggers which was broken in e075e3ccec7387f801333f1c627e9f5f72987267.

src/modules/core/triggers.c
@@ -126,7 +126,7 @@ expire_triggers(void)
 		if (get_time() < (rec->added + expiry))
 			continue;
 
-		if (!rec->t->expiry || get_time() < (rec->added + rec->t->expiry))
+		if (rec->t->expiry && get_time() < (rec->added + rec->t->expiry))
 			continue;
 
 		run_triggers(ACTION_UNBAN, rec->t, &rec->pkt, &rec->irec);