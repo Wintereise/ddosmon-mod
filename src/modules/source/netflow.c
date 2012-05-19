1bc1562c185003c5141295c0bf7a19146ee52263
William Pitcock <nenolod@dereferenced.org>
2012-05-19 03:15:59 -0500

    source/netflow: more crap from nfdump (issue #2)

--- src/modules/source/netflow.c
+++ src/modules/source/netflow.c
@@ -232,6 +232,11 @@ typedef struct {
 	input_translation_t *current_table;
 } exporter_t;
 
+static struct input_table_s {
+	uint16_t offset;
+	uint16_t length;
+} input_template[128];
+
 /* module limited globals */
 static struct element_info_s {
 	// min number of bytes
@@ -409,6 +414,401 @@ translation_table_find(exporter_t *exporter, uint16_t id)
 	exporter->current_table = table;
 	return table;
 }
+
+static inline uint16_t
+element_length_clamp(int element, uint16_t in_length)
+{
+	if (in_length == 0)
+		return 0;
+	if (in_length <= element_info[element].min)
+		return element_info[element].min;
+	if (in_length <= element_info[element].max)
+		return element_info[element].max;
+
+	return 0;
+}
+
+static inline void
+element_fill(input_translation_t *table, int element, uint32_t *offset)
+{
+	uint16_t output_length;
+	uint32_t input_index = table->input_index;
+	uint32_t zero_index  = table->zero_index;
+
+	output_length = element_length_clamp(element, input_template[element].length);
+	if (output_length)
+	{ 
+		DPRINTF("Index: %u v9 id %i, Input Offset %u, Output Offset %u, ilen: %u, olen: %u\n", 
+			input_index, element, input_template[element].offset, *offset, input_template[element].length, output_length);
+
+		table->element[input_index].output_offset 	= *offset;
+		table->element[input_index].input_offset 	= input_template[element].offset;
+		table->element[input_index].length 		= input_template[element].length;
+
+		(*offset) += output_length;
+		table->input_index++;
+	}
+	else
+	{
+		DPRINTF("Zero: %u, v9: %i,  Output Offset %u, len: %u\n", 
+			zero_index, element, *offset, element_info[element].min);
+
+		table->element[zero_index].output_offset 	= *offset;
+		table->element[zero_index].length 		= element_info[element].min;
+		table->zero_index--;
+
+		(*offset)	+= element_info[element].min;
+	}
+}
+
+#if 0
+static input_translation_t *
+setup_translation_table (exporter_t *exporter, uint16_t id, uint16_t input_record_size)
+{
+	input_translation_t *table;
+	uint32_t			i, ipv6, offset, next_extension;
+	size_t				size_required;
+
+	ipv6 = 0;
+
+	table = GetTranslationTable(exporter, id);
+	if ( !table ) {
+		syslog(LOG_INFO, "Process_v9: [%u] Add template %u", exporter->exporter_id, id);
+		table = add_translation_table(exporter, id);
+		if ( !table ) {
+			return NULL;
+		}
+		// Add an extension map
+		// The number of extensions for this template is currently unknown
+		// Allocate enough space for all configured extensions - some may be unused later
+		// make sure memory is 4byte alligned
+		size_required = Max_num_extensions * sizeof(uint16_t) + sizeof(extension_map_t);
+		size_required = (size_required + 3) &~(size_t)3;
+		extension_map = malloc(size_required);
+		if ( !extension_map ) {
+			syslog(LOG_ERR, "Process_v9: Panic! malloc() error in %s line %d: %s", __FILE__, __LINE__, strerror (errno));
+			return  NULL;
+		}
+		extension_map->type 	   = ExtensionMapType;
+		// Set size to an empty tablee - will be adapted later
+		extension_map->size 	   = sizeof(extension_map_t);
+		extension_map->map_id 	   = INIT_ID;
+		// packed record size still unknown at this point - will be added later
+		extension_map->extension_size = 0;
+
+		table->extension_info.map 	 = extension_map;
+		table->extension_map_changed = 1;
+ 	} else {
+		extension_map = table->extension_info.map;
+
+		// reset size/extension size - it's refreshed automatically
+		extension_map->size 	   	  = sizeof(extension_map_t);
+		extension_map->extension_size = 0;
+
+		dbg_printf("[%u] Refresh template %u\n", exporter->exporter_id, id);
+
+		// very noisy with somee exporters
+		// syslog(LOG_DEBUG, "Process_v9: [%u] Refresh template %u", exporter->exporter_id, id);
+	}
+	// clear current table
+	memset((void *)table->element, 0, Max_num_v9_tags * sizeof(translation_element_t));
+	table->updated  	= time(NULL);
+	table->flags		= 0;
+	table->ICMP_offset	= 0;
+	table->sampler_offset 	= 0;
+	table->sampler_size		= 0;
+	table->engine_offset 	= 0;
+	table->router_ip_offset = 0;
+
+	dbg_printf("[%u] Fill translation table %u\n", exporter->exporter_id, id);
+
+	// fill table
+	table->id 			= id;
+	table->input_index 	= 0;
+	table->zero_index 	= Max_num_v9_tags - 1;
+
+	/* 
+	 * common data block: The common record is expected in the output stream. If not available
+	 * in the template, fill values with 0
+	 */
+
+	// All required extensions
+	offset = BYTE_OFFSET_first;
+	FillElement( table, NF9_FIRST_SWITCHED, &offset);
+	FillElement( table, NF9_LAST_SWITCHED, &offset);
+	FillElement( table, NF9_FORWARDING_STATUS, &offset);
+	FillElement( table, NF9_TCP_FLAGS, &offset);
+	FillElement( table, NF9_IN_PROTOCOL, &offset);
+	FillElement( table, NF9_SRC_TOS, &offset);
+	FillElement( table, NF9_L4_SRC_PORT, &offset);
+	FillElement( table, NF9_L4_DST_PORT, &offset);
+
+	/* IP addresss record
+	 * This record is expected in the output stream. If not available
+	 * in the template, assume empty v4 address.
+	 */
+	if ( input_template[NF9_IPV4_SRC_ADDR].length ) {
+		// IPv4 addresses 
+		FillElement( table, NF9_IPV4_SRC_ADDR, &offset);
+		FillElement( table, NF9_IPV4_DST_ADDR, &offset);
+	} else if ( input_template[NF9_IPV6_SRC_ADDR].length == 16 ) {
+		// IPv6 addresses 
+		FillElement( table, NF9_IPV6_SRC_ADDR, &offset);
+		FillElement( table, NF9_IPV6_DST_ADDR, &offset);
+		// mark IPv6 
+		table->flags	|= FLAG_IPV6_ADDR;
+		ipv6 = 1;
+	} else {
+		// should not happen, assume empty IPv4 addresses
+		FillElement( table, NF9_IPV4_SRC_ADDR, &offset);
+		FillElement( table, NF9_IPV4_DST_ADDR, &offset);
+	}
+
+	/* packet counter
+	 * This record is expected in the output stream. If not available
+	 * in the template, assume empty 4 bytes value
+	 */
+	if ( input_template[NF9_IN_PACKETS].length ) {
+		table->packet_offset = offset;
+		FillElement( table, NF9_IN_PACKETS, &offset);
+		// fix: always have 64byte counters if ( input_template[NF9_IN_PACKETS].length == 8 )
+		SetFlag(table->flags, FLAG_PKG_64);
+	} else
+		table->packet_offset = 0;
+
+	/* byte record
+	 * This record is expected in the output stream. If not available
+	 * in the template, assume empty 4 bytes value
+	 */
+	if ( input_template[NF9_IN_BYTES].length ) {
+		table->byte_offset = offset;
+		FillElement( table, NF9_IN_BYTES, &offset);
+		// fix: always have 64byte counters if ( input_template[NF9_IN_BYTES].length == 8 )
+		SetFlag(table->flags, FLAG_BYTES_64);
+	} else 
+		table->byte_offset = 0;
+
+
+	// Optional extensions
+	next_extension = 0;
+	for (i=4; i <= Max_num_extensions; i++ ) {
+		uint32_t map_index = i;
+
+		if ( map_table[i] == 0 )
+			continue;
+
+		switch(i) {
+			case EX_IO_SNMP_2:
+			case EX_IO_SNMP_4:
+				// 2 byte length
+				if ( input_template[NF9_INPUT_SNMP].length <= 2 || input_template[NF9_OUTPUT_SNMP].length <= 2) {
+					FillElement( table, NF9_INPUT_SNMP, &offset);
+					FillElement( table, NF9_OUTPUT_SNMP, &offset);
+					map_index = EX_IO_SNMP_2;
+				} else {
+					FillElement( table, NF9_INPUT_SNMP, &offset);
+					FillElement( table, NF9_OUTPUT_SNMP, &offset);
+					map_index = EX_IO_SNMP_4;
+				}
+				break;
+			case EX_AS_2:
+			case EX_AS_4:
+				if ( input_template[NF9_SRC_AS].length <= 2 || input_template[NF9_DST_AS].length <= 2) {
+					table->src_as_offset = offset;
+					FillElement( table, NF9_SRC_AS, &offset);
+					table->dst_as_offset = offset;
+					FillElement( table, NF9_DST_AS, &offset);
+					map_index = EX_AS_2;
+				} else {
+					table->src_as_offset = offset;
+					FillElement( table, NF9_SRC_AS, &offset);
+					table->dst_as_offset = offset;
+					FillElement( table, NF9_DST_AS, &offset);
+					map_index = EX_AS_4;
+				}
+				break;
+			case EX_MULIPLE:
+				if ( ipv6 ) {
+					// IPv6
+					FillElement( table, NF9_DST_TOS, &offset);
+					FillElement( table, NF9_DIRECTION, &offset);
+					FillElement( table, NF9_IPV6_SRC_MASK, &offset);
+					FillElement( table, NF9_IPV6_DST_MASK, &offset);
+				} else {
+					// IPv4
+					FillElement( table, NF9_DST_TOS, &offset);
+					FillElement( table, NF9_DIRECTION, &offset);
+					FillElement( table, NF9_SRC_MASK, &offset);
+					FillElement( table, NF9_DST_MASK, &offset);
+				}
+				break;
+			case EX_NEXT_HOP_v4:
+				FillElement( table, NF9_V4_NEXT_HOP, &offset);
+				map_index = EX_NEXT_HOP_v4;
+				break;
+			case EX_NEXT_HOP_v6:
+				FillElement( table, NF9_V6_NEXT_HOP, &offset);
+				map_index = EX_NEXT_HOP_v6;
+				table->flags	|= FLAG_IPV6_NH;
+				break;
+			case EX_NEXT_HOP_BGP_v4:
+				FillElement( table, NF9_BGP_V4_NEXT_HOP, &offset);
+				map_index = EX_NEXT_HOP_BGP_v4;
+				break;
+			case EX_NEXT_HOP_BGP_v6:
+				FillElement( table, NF9_BPG_V6_NEXT_HOP, &offset);
+				map_index = EX_NEXT_HOP_BGP_v6;
+				table->flags	|= FLAG_IPV6_NHB;
+				break;
+			case EX_VLAN:
+				FillElement( table, NF9_SRC_VLAN, &offset);
+				FillElement( table, NF9_DST_VLAN, &offset);
+				break;
+			case EX_OUT_PKG_4:
+			case EX_OUT_PKG_8:
+				if ( input_template[NF9_OUT_PKTS].length <= 4 ) {
+					FillElement( table, NF9_OUT_PKTS, &offset);
+					map_index = EX_OUT_PKG_4;
+				} else {
+					FillElement( table, NF9_OUT_PKTS, &offset);
+					map_index = EX_OUT_PKG_8;
+				}
+				break;
+			case EX_OUT_BYTES_4:
+			case EX_OUT_BYTES_8:
+				if ( input_template[NF9_OUT_BYTES].length <= 4 ) {
+					FillElement( table, NF9_OUT_BYTES, &offset);
+					map_index = EX_OUT_BYTES_4;
+				} else {
+					FillElement( table, NF9_OUT_BYTES, &offset);
+					map_index = EX_OUT_BYTES_8;
+				}
+				break;
+			case EX_AGGR_FLOWS_4:
+			case EX_AGGR_FLOWS_8:
+				if ( input_template[NF9_FLOWS_AGGR].length <= 4 ) {
+					FillElement( table, NF9_FLOWS_AGGR, &offset);
+					map_index = EX_AGGR_FLOWS_4;
+				} else {
+					FillElement( table, NF9_FLOWS_AGGR, &offset);
+					map_index = EX_AGGR_FLOWS_8;
+				}
+				break;
+			case EX_MAC_1:
+				FillElement( table, NF9_IN_SRC_MAC, &offset);
+				FillElement( table, NF9_OUT_DST_MAC, &offset);
+				break;
+			case EX_MAC_2:
+				FillElement( table, NF9_IN_DST_MAC, &offset);
+				FillElement( table, NF9_OUT_SRC_MAC, &offset);
+				break;
+			case EX_MPLS:
+				FillElement( table, NF9_MPLS_LABEL_1, &offset);
+				FillElement( table, NF9_MPLS_LABEL_2, &offset);
+				FillElement( table, NF9_MPLS_LABEL_3, &offset);
+				FillElement( table, NF9_MPLS_LABEL_4, &offset);
+				FillElement( table, NF9_MPLS_LABEL_5, &offset);
+				FillElement( table, NF9_MPLS_LABEL_6, &offset);
+				FillElement( table, NF9_MPLS_LABEL_7, &offset);
+				FillElement( table, NF9_MPLS_LABEL_8, &offset);
+				FillElement( table, NF9_MPLS_LABEL_9, &offset);
+				FillElement( table, NF9_MPLS_LABEL_10, &offset);
+				break;
+			case EX_ROUTER_IP_v4:
+			case EX_ROUTER_IP_v6:
+				if ( exporter->sa_family == PF_INET6 ) {
+					table->router_ip_offset = offset;
+					dbg_printf("Router IPv6: Offset: %u, olen: %u\n", offset, 16 );
+					// not an entry for the translateion table.
+					// but reserve space in the output record for IPv6
+					offset			 	   += 16;
+					SetFlag(table->flags, FLAG_IPV6_EXP);
+					map_index = EX_ROUTER_IP_v6;
+				} else {
+					table->router_ip_offset = offset;
+					dbg_printf("Router IPv4: Offset: %u, olen: %u\n", offset, 4 );
+					// not an entry for the translateion table.
+					// but reserve space in the output record for IPv4
+					offset				   += 4;
+					ClearFlag(table->flags, FLAG_IPV6_EXP);
+					map_index = EX_ROUTER_IP_v4;
+				}
+				break;
+			case EX_ROUTER_ID:
+				table->engine_offset = offset;
+				dbg_printf("Engine offset: %u\n", offset);
+				offset += 2;
+				dbg_printf("Skip 2 unused bytes. Next offset: %u\n", offset);
+				FillElement( table, NF9_ENGINE_TYPE, &offset);
+				FillElement( table, NF9_ENGINE_ID, &offset);
+				// unused fill element for 32bit alignment
+				break;
+		}
+		extension_map->size += sizeof(uint16_t);
+		extension_map->extension_size += extension_descriptor[map_index].size;
+
+		// found extension in map_index must be the same as in map - otherwise map is dirty
+		if ( extension_map->ex_id[next_extension] != map_index ) {
+			// dirty map - needs to be refreshed in output stream
+			extension_map->ex_id[next_extension] = map_index;
+			table->extension_map_changed = 1;
+
+		}
+		next_extension++;
+
+	}
+	extension_map->ex_id[next_extension++] = 0;
+
+	// make sure map is aligned
+	if ( extension_map->size & 0x3 ) {
+		extension_map->ex_id[next_extension] = 0;
+		extension_map->size = ( extension_map->size + 3 ) &~ 0x3;
+	}
+
+	table->output_record_size = offset;
+	table->input_record_size  = input_record_size;
+
+	/* ICMP hack for v9  */
+	if ( input_template[NF9_ICMP_TYPE].offset != 0 ) {
+		if ( input_template[NF9_ICMP_TYPE].length == 2 ) 
+			table->ICMP_offset = input_template[NF9_ICMP_TYPE].offset;
+		else
+			syslog(LOG_ERR, "Process_v9: Unexpected ICMP type field length: %d", 
+				input_template[NF9_ICMP_TYPE].length);
+	}
+
+	/* Sampler ID */
+	if ( input_template[NF9_FLOW_SAMPLER_ID].offset != 0 ) {
+		if ( input_template[NF9_FLOW_SAMPLER_ID].length == 1 ) {
+			table->sampler_offset = input_template[NF9_FLOW_SAMPLER_ID].offset;
+			table->sampler_size = 1;
+			dbg_printf("1 byte Sampling ID included at offset %u\n", table->sampler_offset);
+		} else if ( input_template[NF9_FLOW_SAMPLER_ID].length == 2 ) {
+			table->sampler_offset = input_template[NF9_FLOW_SAMPLER_ID].offset;
+			table->sampler_size = 2;
+			dbg_printf("2 byte Sampling ID included at offset %u\n", table->sampler_offset);
+		}  else {
+			syslog(LOG_ERR, "Process_v9: Unexpected SAMPLER ID field length: %d", 
+				input_template[NF9_FLOW_SAMPLER_ID].length);
+			dbg_printf("Unexpected SAMPLER ID field length: %d", 
+				input_template[NF9_FLOW_SAMPLER_ID].length);
+
+		}
+	} else {
+		dbg_printf("No Sampling ID found\n");
+	}
+
+#ifdef DEBUG
+	DPRINTF("Table %u Flags: %u, index: %u, Zero: %u input_size: %u, output_size: %u\n", 
+		table->id, table->flags, table->input_index, table->zero_index, table->input_record_size, table->output_record_size);
+
+	DPRINTF("Process_v9: Check extension map: id: %d, size: %u, extension_size: %u\n", 
+		extension_map->map_id, extension_map->size, extension_map->extension_size);
+#endif
+
+	return table;
+}
+#endif
 
 static exporter_t *
 exporter_correlate(uint32_t source_id)
